512e9c19620590fbddd9b1e2dd7a6097fe3b6f86
Evina88 <evinamous@gmail.com>
2024-03-07 16:02:27 +0200

    ListGroup component and Fragment

--- src/Message.tsx
+++
@@ -1,9 +0,0 @@
-// PascalCasing for function components
-
-function Message() {
-    // JSX: JavaScript XML
-    const name = "Everyone!";
-    return <h1>Hello {name}</h1>;
-}
-
-export default Message;
