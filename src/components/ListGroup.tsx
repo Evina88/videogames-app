a416a2f88a34cd451c562a7281abb738066e27f0
Evina88 <evinamous@gmail.com>
2024-03-07 17:18:36 +0200

    if empty

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,12 @@
 function ListGroup() {
-	const colors = ["red", "blue", "green", "yellow", "orange"];
+	let colors = ["red", "blue", "green", "yellow", "orange"];
+    colors = [];
 
 	return (
 		// syntax for fragments: <></>
 		<>
 			<h1>List</h1>
+            {colors.length === 0 && <p>No colors available</p>}
 			<ul className="list-group">
 				{colors.map((color) => (
 					<li key={color}>{color}</li>
