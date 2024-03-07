c13fc8386d0575257acedca3d15f6652cb6efe97
Evina88 <evinamous@gmail.com>
2024-03-07 17:06:45 +0200

    ListGroup with map()

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,14 @@
 function ListGroup() {
+	const colors = ["red", "blue", "green", "yellow", "orange"];
+
 	return (
 		// syntax for fragments: <></>
 		<>
 			<h1>List</h1>
 			<ul className="list-group">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-				<li className="list-group-item">A fourth item</li>
-				<li className="list-group-item">And a fifth one</li>
+				{colors.map((color) => (
+					<li key={color}>{color}</li>
+				))}
 			</ul>
 		</>
 	);
